8cff475e18540000334583e5ad82c890d5ea03b6
Padraig Crotty <46968070+Crotshot@users.noreply.github.com>
2020-10-28 12:06:40 +0000

    Added routing and live data from API

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
+<Route path="/movies/:id" component={MoviePage} />
 
 const App = () => {
   return (
