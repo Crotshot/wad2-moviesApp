4e97e743ea46d27ccc091f57bf545af53a896a54
Padraig Crotty <46968070+Crotshot@users.noreply.github.com>
2020-11-04 11:06:40 +0000

    Show movie review excerpts; Refactor movie page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -6,14 +6,9 @@ import { getMovies } from "../api/tmdb-api";
 const MovieListPage = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results )
-      .then(movies => {
-        setMovies(movies);
-      });
+    getMovies().then(movies => {
+      setMovies(movies);
+    });
   }, []);
 
   const addToFavorites = movieId => {
